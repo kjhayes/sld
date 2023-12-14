12fa58d8f4077d99daa7d7213f235d98d4bff271
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2023-12-14 16:12:52 -0600

    Creating an Output File

---
+++ src/linker.rs
@@ -0,0 +1,12 @@
+
+use std::fs;
+use std::io;
+use std::io::Write;
+
+use object::{write, Object, ObjectKind, File};
+
+pub fn link_into(out: &mut write::Object, inputs: &Vec<File>)
+{
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 
+mod linker;
+
 use std::fs;
+use std::io;
+use std::io::Write;
 use std::process::exit;
 use clap::Parser;
 
-use object::{Object, ObjectSection, ObjectKind};
+use object::{write, Object, ObjectKind, File};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -26,20 +30,20 @@ fn get_input_file_contents(input_files: &Vec<String>) -> Vec<Vec<u8>>
     return file_content_buf;
 }
 
-fn parse_input_files(file_content_buf: &Vec<Vec<u8>>) -> Vec<object::File>
+fn parse_input_files(file_content_buf: &Vec<Vec<u8>>) -> Vec<File>
 {
     let mut obj_files = vec![];
     for file_contents in file_content_buf
     {
         let bytes: &[u8] = file_contents;
-        let obj_file = object::File::parse(bytes)
+        let obj_file = File::parse(bytes)
             .expect("Could not parse input object file");
         obj_files.push(obj_file);
     }
     return obj_files;
 }
 
-fn check_object_kinds(obj_files: &Vec<object::File>, input_files: &Vec<String>) -> u32 
+fn check_object_kinds(obj_files: &Vec<File>, input_files: &Vec<String>) -> u32 
 {
     let mut m_invalid_kinds = 0;
     for (i, &ref obj_file) in obj_files.iter().enumerate() {
@@ -53,14 +57,14 @@ fn check_object_kinds(obj_files: &Vec<object::File>, input_files: &Vec<String>) 
     return m_invalid_kinds;
 }
 
-fn check_object_incompats(obj_files: &Vec<object::File>, input_files: &Vec<String>) -> u32 
+fn check_object_incompats(obj_files: &Vec<File>, input_files: &Vec<String>) -> (u32,object::Architecture,object::Endianness)
 {
     // Get the traits which must be compatible in the first object file
     let mut m_incompat = 0;
     let o = &obj_files[0];
     let o_name = &input_files[0];
     let o_arch = o.architecture();
-    let o_endian = o.is_little_endian();
+    let o_endian = if o.is_little_endian() {object::Endianness::Little} else {object::Endianness::Big};
     let o_class = o.is_64();
 
     for (i, &ref obj_file) in obj_files.iter().enumerate() 
@@ -68,7 +72,7 @@ fn check_object_incompats(obj_files: &Vec<object::File>, input_files: &Vec<Strin
         let curr_name = &input_files[i];
 
         let curr_arch = obj_file.architecture();
-        let curr_endian = obj_file.is_little_endian();
+        let curr_endian = if obj_file.is_little_endian() {object::Endianness::Little} else {object::Endianness::Big};
         let curr_class = obj_file.is_64();
 
         if curr_arch != o_arch {
@@ -87,7 +91,7 @@ fn check_object_incompats(obj_files: &Vec<object::File>, input_files: &Vec<Strin
                 curr_name, o_name, curr_class, o_class);
         }
     }
-    return m_incompat;
+    return (m_incompat,o_arch,o_endian);
 }
 
 fn main() {
@@ -110,12 +114,28 @@ fn main() {
         exit(1);
     }
     // Validate that all object files are compatible
-    let num_incompat = check_object_incompats(&obj_files, &args.input_files);
+    let (num_incompat,arch,endian) = check_object_incompats(&obj_files, &args.input_files);
     if num_incompat > 0 {
         exit(1);
     }
 
+    let mut output_obj = write::Object::new(object::BinaryFormat::Elf,arch,endian); 
 
+    linker::link_into(&mut output_obj, &obj_files);
+
+    let output_bytes = output_obj.write()
+        .expect("Error creating output file contents");
+
+    let mut output_file = fs::OpenOptions::new()
+        .write(true)
+        .read(false)
+        .truncate(true)
+        .create(true)
+        .open(args.output)
+        .expect("Could not open or create output file");
+
+    output_file.write_all(&output_bytes)
+        .expect("Could not write to output file");
 
     exit(0);
 }
